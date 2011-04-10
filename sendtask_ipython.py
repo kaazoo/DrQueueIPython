3099310966956658fd2253fdca2cd554b56db452
Andreas Schroeder <andreas@drqueue.org>
2011-04-11 01:13:44 +0200

    create multiple tasks for one job

--- sendtask_ipython.py
+++ sendtask_ipython.py
@@ -40,32 +40,41 @@ def main():
                       action="store_false", dest="verbose", default=True,
                       help="verbose output")
     (options, args) = parser.parse_args()
-        
-    # prepare script input
-    push_dict = {
-    'DRQUEUE_OS' : get_osname(),
-    'DRQUEUE_ETC' : os.getenv('DRQUEUE_ROOT') + "/etc",
-    'DRQUEUE_FRAME' : int(options.startframe),
-    'DRQUEUE_BLOCKSIZE' : int(options.blocksize),
-    'DRQUEUE_ENDFRAME' : int(options.endframe),
-    'SCENE' : options.scenefile,
-    'RENDER_TYPE' : "animation"
-    }
 
-    # prepare task
-    work = "execfile(\"" + os.getenv('DRQUEUE_ROOT') + "/etc/" + get_rendertemplate(options.renderer) + "\")"
-    mytask = StringTask(work, push=push_dict, clear_before=True)
-    
-    # run task on cluster
+    # initialize IPython
     tc = client.TaskClient()
-    task_id = tc.run(mytask)
-    
-    task_results = tc.get_task_result(task_id, True)
-    print("Task %i was running on %i and took %f seconds to run." % (task_id,task_results.engineid,task_results.duration))
-    
-    if task_results.failure:
-        print("task failed:")
-        print(task_results.failure)
+
+    task_ids = list()
+    task_frames = range(int(options.startframe), int(options.endframe)+1, int(options.blocksize))
+
+    for x in task_frames:
+        # prepare script input
+        push_dict = {
+        'DRQUEUE_OS' : get_osname(),
+        'DRQUEUE_ETC' : os.getenv('DRQUEUE_ROOT') + "/etc",
+        'DRQUEUE_FRAME' : x,
+        'DRQUEUE_BLOCKSIZE' : int(options.blocksize),
+        'DRQUEUE_ENDFRAME' : int(options.endframe),
+        'SCENE' : options.scenefile,
+        'RENDER_TYPE' : "animation"
+        }
+
+        # prepare task
+        work = "execfile(\"" + os.getenv('DRQUEUE_ROOT') + "/etc/" + get_rendertemplate(options.renderer) + "\")"
+        mytask = StringTask(work, push=push_dict, clear_before=True)
+
+        # run task on cluster
+
+        task_id = tc.run(mytask)
+        task_ids.append(task_id)
+
+    for x in task_ids:
+        task_results = tc.get_task_result(x, True)
+        print("Task %i was running on %i and took %f seconds to run." % (x,task_results.engineid,task_results.duration))
+
+        if task_results.failure:
+            print("task failed:")
+            print(task_results.failure)
 
 
 if __name__ == "__main__":
