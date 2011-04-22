15fbad87c65a4c6c072279845c49854c833c36bc
Andreas Schroeder <andreas@drqueue.org>
2011-04-23 01:52:27 +0200

    add methods for job actions

--- DrQueue/client.py
+++ DrQueue/client.py
@@ -45,10 +45,10 @@ class Client():
 
         # fetch list of jobs
         for entry in entries:
-            emsg_id = entry['msg_id']
-            eheader = entry['header']
+            msg_id = entry['msg_id']
+            header = entry['header']
 
-            if eheader['after'] != []:
+            if header['after'] != []:
                 jobs.append(entry)
         return jobs
 
@@ -57,3 +57,64 @@ class Client():
         dict = {'msg_id': task_id }
         task = self.ip_client.db_query(dict)[0]
         return task
+
+    """Stop job and all tasks which are not currently running"""
+    def job_stop(self, job_id):
+        dict = {'msg_id': job_id }
+        job = self.ip_client.db_query(dict)[0]
+        header = job['header']
+
+        # abort all queued tasks
+        for task_id in header['after']:
+            self.ip_client.abort(task_id)
+
+        # abort job as well
+        self.ip_client.abort(job_id)
+        return True
+
+    """Stop job and all of it's tasks wether running or not"""
+    def job_kill(self, job_id):
+        dict = {'msg_id': job_id }
+        job = self.ip_client.db_query(dict)[0]
+        header = job['header']
+
+        # abort all queued tasks
+        for task_id in header['after']:
+            self.lbview.abort(task_id)
+
+        # abort job as well
+        self.lbview.abort(job_id)
+        return True
+
+
+    """Delete job and all of it's tasks"""
+    def job_delete(self, job_id):
+        dict = {'msg_id': job_id }
+        job = self.ip_client.db_query(dict)[0]
+        header = job['header']
+
+        # abort and delete all queued tasks
+        for task_id in header['after']:
+            self.lbview.abort(task_id)
+            self.ip_client.purge_results(task_id)
+
+        # abort and delete job as well
+        self.lbview.abort(job_id)
+        self.ip_client.purge_results(job_id)
+        return True
+
+    def job_continue(self, job_id):
+        return True
+
+    """Return status string of job"""
+    def job_status(self, job_id):
+        job = self.query_task(job_id)
+
+        if job['completed'] == None:
+            status = "pending"
+        else:
+            result_header = job['result_header']
+            status = result_header['status']
+        return status
+
+
