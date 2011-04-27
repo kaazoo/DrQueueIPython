2580c5c553f62676112203dec09b211bf2356dd9
Andreas Schroeder <andreas@drqueue.org>
2011-04-28 01:33:20 +0200

    remove old comments variables from IPython become upper case local variables become lower case

--- etc/blender_sg.py
+++ etc/blender_sg.py
@@ -1,70 +1,38 @@
 # -*- coding: utf-8 -*-
 
-#
-# THIS IS A PYTHON SCRIPT FILE
-# 
-# Default configuration for Blender script generator
-# 
-# Python variables
-# SCENE, RENDER_TYPE
-# 
-# shell variables
-# DRQUEUE_BLOCKSIZE, DRQUEUE_COMPID, DRQUEUE_ENDFRAME, DRQUEUE_ETC, DRQUEUE_FRAME,
-# DRQUEUE_JOBID, DRQUEUE_JOBNAME, DRQUEUE_OS, DRQUEUE_OWNER, DRQUEUE_PADFRAME, 
-# DRQUEUE_PADFRAMES, DRQUEUE_STARTFRAME, DRQUEUE_STEPFRAME
-#
-
-#
-# For platform dependend environment setting a form like this
-# can be used :
-#
-# if DRQUEUE_OS == "LINUX":
-#    # Environment for Linux
-# elsif DRQUEUE_OS == "IRIX":
-#    # Environment for Irix
-# else
-#    # Some error messages
-#
-
 import os,signal,subprocess,sys
 
 os.umask(0)
 
-# fetch DrQueue environment
-#DRQUEUE_BLOCKSIZE = int(os.getenv("DRQUEUE_BLOCKSIZE"))
-#DRQUEUE_COMPID = int(os.getenv("DRQUEUE_COMPID"))
-#DRQUEUE_ENDFRAME = int(os.getenv("DRQUEUE_ENDFRAME"))
-#DRQUEUE_ETC = os.getenv("DRQUEUE_ETC")
-#DRQUEUE_FRAME = int(os.getenv("DRQUEUE_FRAME"))
-#DRQUEUE_JOBID = int(os.getenv("DRQUEUE_JOBID"))
-#DRQUEUE_JOBNAME = os.getenv("DRQUEUE_JOBNAME")
-#DRQUEUE_OS = os.getenv("DRQUEUE_OS")
-#DRQUEUE_OWNER = os.getenv("DRQUEUE_OWNER")
-#DRQUEUE_PADFRAME = int(os.getenv("DRQUEUE_PADFRAME"))
-#DRQUEUE_PADFRAMES = int(os.getenv("DRQUEUE_PADFRAMES"))
-#DRQUEUE_STARTFRAME = int(os.getenv("DRQUEUE_STARTFRAME"))
-#DRQUEUE_STEPFRAME = int(os.getenv("DRQUEUE_STEPFRAME"))
+# external variables in upper case:
+# DRQUEUE_OS
+# DRQUEUE_ETC
+# DRQUEUE_SCENEFILE
+# DRQUEUE_FRAME
+# DRQUEUE_BLOCKSIZE
+# DRQUEUE_ENDFRAME
+# DRQUEUE_RENDERTYPE
 
 
 if DRQUEUE_OS == "WINDOWS":
 	# convert to windows path with drive letter
-	SCENE = subprocess.Popen(["cygpath.exe", "-w "+SCENE], stdout=subprocess.PIPE).communicate()[0]
+	DRQUEUE_SCENE = subprocess.Popen(["cygpath.exe", "-w "+DRQUEUE_SCENEFILE], stdout=subprocess.PIPE).communicate()[0]
 
-BLOCK = DRQUEUE_FRAME + DRQUEUE_BLOCKSIZE - 1
+block = DRQUEUE_FRAME + DRQUEUE_BLOCKSIZE - 1
 
-if BLOCK > DRQUEUE_ENDFRAME:
-	BLOCK = DRQUEUE_ENDFRAME
+if block > DRQUEUE_ENDFRAME:
+	block = DRQUEUE_ENDFRAME
 
-ENGINE_PATH="blender"
+engine_path="blender"
 
-if RENDER_TYPE == "animation":
+if DRQUEUE_RENDERTYPE == "animation":
   os.putenv("startframe", str(DRQUEUE_FRAME))
-  os.putenv("endframe", str(BLOCK))
-  command = ENGINE_PATH+" -b "+SCENE+" -P "+DRQUEUE_ETC+"/blender_same_directory.py"
+  os.putenv("endframe", str(block))
+  command = engine_path+" -b "+DRQUEUE_SCENEFILE+" -P "+DRQUEUE_ETC+"/blender_same_directory.py"
 else:
   os.putenv("curpart", str(DRQUEUE_FRAME))
   os.outenv("maxparts", str(DRQUEUE_ENDFRAME))
-  command = ENGINE_PATH+" -b "+SCENE+" -P "+DRQUEUE_ETC+"/blender_region_rendering.py"
+  command = engine_path+" -b "+DRQUEUE_SCENEFILE+" -P "+DRQUEUE_ETC+"/blender_region_rendering.py"
 
 
 print(command)
@@ -74,11 +42,11 @@ p = subprocess.Popen(command, shell=True)
 sts = os.waitpid(p.pid, 0)
 
 # This should requeue the frame if failed
-if sts[1] != 0:
-	print("Requeueing frame...")
-	os.kill(os.getppid(), signal.SIGINT)
-	exit(1)
-else:
+#if sts[1] != 0:
+#	print("Requeueing frame…")
+#	os.kill(os.getppid(), signal.SIGINT)
+#	exit(1)
+#else:
 	#if DRQUEUE_OS != "WINDOWS" then:
 	# The frame was rendered properly
 	# We don't know the output image name. If we knew we could set this correctly
@@ -86,7 +54,7 @@ else:
 
 	# change userid and groupid
 	#chown 1002:1004 $SCENE:h/*
-	print("Finished.")
+#	print("Finished.")
 #
 # Notice that the exit code of the last command is received by DrQueue
 #
