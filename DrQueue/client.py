b12efa3d46b59a067662e97aa41513b33c02677a
Andreas Schroeder <andreas@drqueue.org>
2011-04-28 01:27:01 +0200

    fix docstrings add optional elements to dict if available

--- DrQueue/client.py
+++ DrQueue/client.py
@@ -13,8 +13,8 @@ class Client():
         self.lbview = self.ip_client.load_balanced_view()
 
 
-    """Wrapper for creating sub tasks and dummy task"""
     def run_job(self, job):
+        """Create tasks for job"""
         # set session name which will be used as job name
         self.ip_client.session.session = job['name']
 
@@ -27,9 +27,42 @@ class Client():
             'DRQUEUE_FRAME' : x,
             'DRQUEUE_BLOCKSIZE' : job['blocksize'],
             'DRQUEUE_ENDFRAME' : job['endframe'],
-            'SCENE' : job['scene'],
-            'RENDER_TYPE' : "animation"
+            'DRQUEUE_SCENE' : job['scenefile'],
             }
+
+            # optional elements
+            if 'renderdir' in job:
+                env_dict['DRQUEUE_RENDERDIR'] = job['renderdir']
+            if 'projectdir' in job:
+                env_dict['DRQUEUE_PROJECTDIR'] = job['projectdir']
+            if 'configdir' in job:
+                env_dict['DRQUEUE_CONFIGDIR'] = job['configdir']
+            if 'imagefile' in job:
+                env_dict['DRQUEUE_IMAGEFILE'] = job['imagefile']
+            if 'precommand' in job:
+                env_dict['DRQUEUE_PRECOMMAND'] = job['precommand']
+            if 'postcommand' in job:
+                env_dict['DRQUEUE_POSTCOMMAND'] = job['postcommand']
+            if 'viewcommand' in job:
+                env_dict['DRQUEUE_VIEWCOMMAND'] = job['viewcommand']
+            if 'worldfile' in job:
+                env_dict['DRQUEUE_WORLDFILE'] = job['worldfile']
+            if 'terrainfile' in job:
+                env_dict['DRQUEUE_TERRAINFILE'] = job['terrainfile']
+            if 'composition' in job:
+                env_dict['DRQUEUE_COMPOSITION'] = job['composition']
+            if 'camera' in job:
+                env_dict['DRQUEUE_CAMERA'] = job['camera']
+            if 'resx' in job:
+                env_dict['DRQUEUE_RESX'] = job['resx']
+            if 'resy' in job:
+                env_dict['DRQUEUE_RESY'] = job['resy']
+            if 'renderpass' in job:
+                env_dict['DRQUEUE_RENDERPASS'] = job['renderpass']
+            if 'rendertype' in job:
+                env_dict['DRQUEUE_RENDERTYPE'] = job['rendertype']
+            if 'fileextension' in job:
+                env_dict['DRQUEUE_FILEEXTENSION'] = job['fileextension']
     
             # run task on cluster
             render_script = os.getenv('DRQUEUE_ROOT') + "/etc/" + DrQueue.get_rendertemplate(job['renderer'])
@@ -38,15 +71,15 @@ class Client():
             time.sleep(0.5)
 
 
-    """Wait for task to finish"""
     def wait_for_task(self, task_id):
+        """Wait for task to finish"""
         ar = self.ip_client.get_result(task_id)
         ar.wait()
         return ar
 
 
-    """Query a list of all jobs (IPython sessions)"""
     def query_all_jobs(self):
+        """Query a list of all jobs (IPython sessions)"""
         jobs = []
         query_data = self.ip_client.db_query({"header.session" : {"$ne" : ""}}, keys=["header.session"])
         for entry in query_data:
@@ -57,21 +90,21 @@ class Client():
         return jobs
 
 
-    """Query a list of tasks objects of certain job"""
     def query_tasks_of_job(self, jobname):
+        """Query a list of tasks objects of certain job"""
         tasks = self.ip_client.db_query({"header.session" : jobname})
         return tasks
 
 
-    """Query a single task"""
     def query_task(self, task_id):
+        """Query a single task"""
         dict = {'msg_id': task_id }
         task = self.ip_client.db_query(dict)[0]
         return task
 
 
-    """Stop job and all tasks which are not currently running"""
     def job_stop(self, jobname):
+        """Stop job and all tasks which are not currently running"""
         tasks = self.query_tasks_of_job(jobname)
 
         # abort all queued tasks
@@ -81,8 +114,8 @@ class Client():
         return True
 
 
-    """Stop job and all of it's tasks wether running or not"""
     def job_kill(self, jobname):
+        """Stop job and all of it's tasks wether running or not"""
         tasks = self.query_tasks_of_job(jobname)
 
         # abort all queued tasks
@@ -92,8 +125,8 @@ class Client():
         return True
 
 
-    """Delete job and all of it's tasks"""
     def job_delete(self, jobname):
+        """Delete job and all of it's tasks"""
         tasks = self.query_tasks_of_job(jobname)
 
         # abort and delete all queued tasks
@@ -105,11 +138,12 @@ class Client():
 
 
     def job_continue(self, jobname):
+        """Continue stopped job"""
         return True
 
 
-    """Return status string of job"""
     def job_status(self, jobname):
+        """Return status string of job"""
         job = self.query_task(job_id)
 
         if job['completed'] == None:
