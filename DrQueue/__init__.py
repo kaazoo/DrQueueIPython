357a60bcc4a3329bc3904e1c5f40502921b17c39
Andreas Schroeder <andreas@drqueue.org>
2011-04-28 01:19:34 +0200

    add more renderer names load variables from input dict

--- DrQueue/__init__.py
+++ DrQueue/__init__.py
@@ -2,24 +2,57 @@
 
 """DrQueue module"""
 
+import platform
 from client import Client
 from job import Job
 
-import platform
-
 
 def get_rendertemplate(renderer):
+    """Return template filename from renderer name"""
     filename = ""
+    if renderer == '3delight':
+        filename = '3delight_sg.py'
+    if renderer == '3dsmax':
+        filename = '3dsmax_sg.py'
+    if renderer == 'aftereffects':
+        filename = 'aftereffects_sg.py'
+    if renderer == 'aqsis':
+        filename = 'aqsis_sg.py'
     if renderer == 'blender':
         filename = 'blender_sg.py'
+    if renderer == 'cinema4d':
+        filename = 'cinema4d_sg.py'
+    if renderer == 'general':
+        filename = 'general_sg.py'
+    if renderer == 'lightwave':
+        filename = 'lightwave_sg.py'
+    if renderer == 'luxrender':
+        filename = 'luxrender_sg.py'
+    if renderer == 'mantra':
+        filename = 'mantra_sg.py'
     if renderer == 'maya':
         filename = 'maya_sg.py'
     if renderer == 'mentalray':
         filename = 'mentalray_sg.py'
+    if renderer == 'nuke':
+        filename = 'nuke_sg.py'
+    if renderer == 'pixie':
+        filename = 'pixie_sg.py'
+    if renderer == 'shake':
+        filename = 'shake_sg.py'
+    if renderer == 'terragen':
+        filename = 'terragen_sg.py'
+    if renderer == 'turtle':
+        filename = 'turtle_sg.py'
+    if renderer == 'vray':
+        filename = 'vray_sg.py'
+    if renderer == 'xsi':
+        filename = 'xsi_sg.py'
     return filename 
 
 
 def get_osname():
+    """Return operating system name"""
     osname = platform.system()
     if osname == 'Darwin':
         osname = 'OSX'
@@ -27,13 +60,9 @@ def get_osname():
 
 
 def run_script_with_env(script, env_dict):
-    DRQUEUE_OS = env_dict['DRQUEUE_OS']
-    DRQUEUE_ETC = env_dict['DRQUEUE_ETC']
-    DRQUEUE_FRAME = env_dict['DRQUEUE_FRAME']
-    DRQUEUE_BLOCKSIZE = env_dict['DRQUEUE_BLOCKSIZE']
-    DRQUEUE_ENDFRAME = env_dict['DRQUEUE_ENDFRAME']
-    SCENE = env_dict['SCENE']
-    RENDER_TYPE = env_dict['RENDER_TYPE']
+    """Create variables and template script on engine"""
+    # load variables from dict
+    locals().update(env_dict)
     return execfile(script)
 
 
@@ -41,3 +70,4 @@ def run_dummy():
     import time
     time.sleep(1)
     return True
+
