78bbdb26639b8844a1523517f0b2144d6a69edb9
Andreas Schroeder <andreas@drqueue.org>
2011-05-05 02:06:01 +0200

    add option for job rerun

--- controljob_ipython.py
+++ controljob_ipython.py
@@ -13,17 +13,19 @@ def main():
     parser.usage = "%prog [options] -n name"
     parser.add_option("-n", "--name ",
                       dest="name", help="name of job")
-    parser.add_option("-s", "--stop", 
+    parser.add_option("-s", "--stop",
                       action="store_true", dest="stop", default=False, help="stop the job")
-    parser.add_option("-k", "--kill", 
+    parser.add_option("-k", "--kill",
                       action="store_true", dest="kill", default=False, help="stop the job (be careful, running tasks will be killed)")
-    parser.add_option("-d", "--delete", 
+    parser.add_option("-d", "--delete",
                       action="store_true", dest="delete", default=False, help="delete the job (be careful, no confirmation asked)")
-    parser.add_option("-c", "--continue", 
+    parser.add_option("-c", "--continue",
                       action="store_true", dest="cont", default=False, help="continue a previously stopped job")
-    parser.add_option("-t", "--status", 
+    parser.add_option("-r", "--rerun",
+                      action="store_true", dest="rerun", default=False, help="rerun job")
+    parser.add_option("-t", "--status",
                       action="store_true", dest="status", default=False, help="show the status of the job")
-    parser.add_option("-v", "--verbose", 
+    parser.add_option("-v", "--verbose",
                       action="store_true", dest="verbose", default=False, help="verbose output")
     (options, args) = parser.parse_args()
 
@@ -46,7 +48,11 @@ def main():
     if options.cont:
         client.job_continue(options.name)
         print("Job %s is running again." % options.name)
-        return     
+        return
+    if options.rerun:
+        client.job_rerun(options.name)
+        print("Job %s is running another time." % options.name)
+        return
     if options.status:
         status = client.job_status(options.name)
         print("The status of job %s is \"%s\"" % (options.name, status))
