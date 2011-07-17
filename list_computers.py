d24d10081dc19b08bd158d761df0f9a4dc84f717
Andreas Schroeder <andreas@drqueue.org>
2011-07-17 15:22:07 +0200

    demo script for listing computer information

---
+++ list_computers.py
@@ -0,0 +1,27 @@
+# -*- coding: utf-8 -*-
+
+"""
+List information about connected computers
+Copyright (C) 2011 Andreas Schroeder
+
+This file is part of DrQueue.
+
+Licensed under GNU General Public License version 3. See LICENSE for details.
+"""
+
+from DrQueue import Client
+
+c = Client()
+
+for comp_id in c.ip_client.ids:
+    print("Engine "+str(comp_id)+ ":")
+    comp = c.identify_computer(comp_id)
+    print(" hostname: "+comp['hostname'])
+    print(" arch: "+comp['arch'])
+    print(" os: "+comp['os'])
+    print(" nbits: "+str(comp['nbits']))
+    print(" procspeed: "+comp['procspeed'])
+    print(" ncpus: "+str(comp['ncpus']))
+    print(" ncorescpu: "+str(comp['ncorescpu']))
+    print(" memory: "+comp['memory']+"\n")
+
