4a9fe56fd87ab39a588222b81d126b5bf4824403
Andreas Schroeder <andreas@drqueue.org>
2011-08-15 23:11:53 +0200

    update render template for Cinema4D

--- etc/cinema4d_sg.py
+++ etc/cinema4d_sg.py
@@ -1,122 +1,72 @@
-#
-# THIS IS A PYTHON SCRIPT FILE
-# 
-# Default configuration for Cinema4D script generator
-# 
-# Python variables
-# SCENE, RENDERDIR, RF_OWNER
-# 
-# shell variables
-# DRQUEUE_BLOCKSIZE, DRQUEUE_COMPID, DRQUEUE_ENDFRAME, DRQUEUE_ETC, DRQUEUE_FRAME,
-# DRQUEUE_JOBID, DRQUEUE_JOBNAME, DRQUEUE_OS, DRQUEUE_OWNER, DRQUEUE_PADFRAME, 
-# DRQUEUE_PADFRAMES, DRQUEUE_STARTFRAME, DRQUEUE_STEPFRAME
-#
-
-#
-# For platform dependend environment setting a form like this
-# can be used :
-#
-# if DRQUEUE_OS == "LINUX":
-#    # Environment for Linux
-# elsif DRQUEUE_OS == "IRIX":
-#    # Environment for Irix
-# else
-#    # Some error messages
-#
+# -*- coding: utf-8 -*-
 
 import os,signal,subprocess,sys
+import os.path
+from time import strftime,localtime
 
-os.umask(0)
 
-# fetch DrQueue environment
-DRQUEUE_BLOCKSIZE = int(os.getenv("DRQUEUE_BLOCKSIZE"))
-DRQUEUE_COMPID = int(os.getenv("DRQUEUE_COMPID"))
-DRQUEUE_ENDFRAME = int(os.getenv("DRQUEUE_ENDFRAME"))
-DRQUEUE_ETC = os.getenv("DRQUEUE_ETC")
-DRQUEUE_FRAME = int(os.getenv("DRQUEUE_FRAME"))
-DRQUEUE_JOBID = int(os.getenv("DRQUEUE_JOBID"))
-DRQUEUE_JOBNAME = os.getenv("DRQUEUE_JOBNAME")
-DRQUEUE_OS = os.getenv("DRQUEUE_OS")
-DRQUEUE_OWNER = os.getenv("DRQUEUE_OWNER")
-DRQUEUE_PADFRAME = int(os.getenv("DRQUEUE_PADFRAME"))
-DRQUEUE_PADFRAMES = int(os.getenv("DRQUEUE_PADFRAMES"))
-DRQUEUE_STARTFRAME = int(os.getenv("DRQUEUE_STARTFRAME"))
-DRQUEUE_STEPFRAME = int(os.getenv("DRQUEUE_STEPFRAME"))
-
+# external variables in upper case:
+# DRQUEUE_OS
+# DRQUEUE_ETC
+# DRQUEUE_SCENEFILE
+# DRQUEUE_FRAME
+# DRQUEUE_BLOCKSIZE
+# DRQUEUE_ENDFRAME
+# DRQUEUE_RENDERDIR
+# DRQUEUE_RENDERTYPE
+# DRQUEUE_LOGFILE
 
 if DRQUEUE_OS == "WINDOWS":
-	# convert to windows path with drive letter
-	SCENE=subprocess.Popen(["cygpath.exe", "-w "+SCENE], stdout=subprocess.PIPE).communicate()[0]
-	RENDERDIR=subprocess.Popen(["cygpath.exe", "-w "+RENDERDIR], stdout=subprocess.PIPE).communicate()[0]
-	
-	ENGINE_PATH="C:\Program\ Files\MAXON\CINEMA\ 4D\ R10\CINEMA\ 4D.exe"
-	LOGFILE="C:\Program\ Files\MAXON\CINEMA\ 4D\ R10\RENDERLOG.TXT"
+  	engine_path = "C:\Program\ Files\MAXON\CINEMA\ 4D\ R12\CINEMA\ 4D.exe"
 
 if DRQUEUE_OS == "OSX":
-	ENGINE_PATH="/Applications/MAXON/CINEMA\ 4D\ R10/CINEMA\ 4D.app/Contents/MacOS/CINEMA\ 4D"
-	LOGFILE="/Applications/MAXON/CINEMA\ 4D\ R10/CINEMA\ 4D.app/Contents/MacOS/CINEMA\ 4D/RENDERLOG.TXT"    
+  	engine_path = "/Applications/MAXON/CINEMA\ 4D\ R12/CINEMA\ 4D.app/Contents/MacOS/CINEMA\ 4D"
 
 if DRQUEUE_OS == "LINUX":
-	# we use wine on linux (this is a hack, but works)
-	# there is a tightvnc server running on display :1
-	# see wine bug #8069
-	# the user running DrQueue slave process needs to have wine and Cinema4D installed
-	
-	# convert to windows path with drive letter
-	SCENE=subprocess.Popen(["winepath", "-w "+SCENE], stdout=subprocess.PIPE).communicate()[0]
-	RENDERDIR=subprocess.Popen(["winepath", "-w "+RENDERDIR], stdout=subprocess.PIPE).communicate()[0]
-	
-	WORKDIR="~/.wine/drive_c/Program\ Files/MAXON/CINEMA\ 4D\ R10"
-	ENGINE_PATH="wine CINEMA\ 4D.exe"
-	LOGFILE="~/.wine/drive_c/Program\ Files/MAXON/CINEMA\ 4D\ R10/RENDERLOG.TXT"
-	
-	# change into workdir, better for wine startup
-	os.chdir(WORKDIR)
-	
-	# set env variable, so wine can access the xserver even though we are rendering headless
-	os.environ["DISPLAY"]=":1"
+  	# we use wine on linux (this is a hack, but works)
+  	# there is a tightvnc server running on display :1
+  	# see wine bug #8069
+  	# the user running DrQueue slave process needs to have wine and Cinema4D installed
 
+  	# convert to windows path with drive letter
+  	DRQUEUE_SCENEFILE = subprocess.Popen(["winepath", "-w "+DRQUEUE_SCENEFILE], stdout=subprocess.PIPE).communicate()[0]
+  	DRQUEUE_RENDERDIR = subprocess.Popen(["winepath", "-w "+DRQUEUE_RENDERDIR], stdout=subprocess.PIPE).communicate()[0]
 
-BLOCK = DRQUEUE_FRAME + DRQUEUE_BLOCKSIZE - 1
+  	workdir = "~/.wine/drive_c/Program\ Files/MAXON/CINEMA\ 4D\ R12"
+  	engine_path = "wine CINEMA\ 4D.exe"
 
-if BLOCK > DRQUEUE_ENDFRAME:
-	BLOCK = DRQUEUE_ENDFRAME
+  	# change into workdir, better for wine startup
+  	os.chdir(workdir)
 
+  	# set env variable, so wine can access the xserver even though we are rendering headless
+  	os.environ["DISPLAY"] = ":1"
 
-# delete old logfile
-os.remove(LOGFILE)
+# range to render
+block = DRQUEUE_FRAME + DRQUEUE_BLOCKSIZE - 1
+if block > DRQUEUE_ENDFRAME:
+	block = DRQUEUE_ENDFRAME
 
-command = ENGINE_PATH+"-nogui -render "+SCENE+" -oimage "+RENDERDIR+" -frame "+str(DRQUEUE_FRAME)+" -omultipass "+RENDERDIR+" -threads 0"
+command = engine_path+" -nogui -render "+DRQUEUE_SCENEFILE+" -oimage " + DRQUEUE_RENDERDIR + " -frame " + DRQUEUE_FRAME + " -omultipass " + DRQUEUE_RENDERDIR + " -threads 0"
 
+# write output to logfile
+logfile = open(DRQUEUE_LOGFILE, "ab")
+logfile.write("Log started at " + strftime("%a, %d %b %Y %H:%M:%S", localtime()) + "\n\n")
+logfile.write(command+"\n")
+logfile.flush()
 
-print(command)
-sys.stdout.flush()
+# check scenefile
+if os.path.isfile(DRQUEUE_SCENEFILE) == False:
+    logfile.write("Scenefile was not found. Exiting with status 1.\n\n")
+    logfile.close()
+    exit(1)
 
-p = subprocess.Popen(command, shell=True)
+# run renderer and wait for finish
+p = subprocess.Popen(command, shell=True, stdout=logfile, stderr=subprocess.STDOUT)
 sts = os.waitpid(p.pid, 0)
 
-# display contents of logfile
-file = open(LOGFILE)
-while 1:
-    line = file.readline()
-    if not line:
-        break
-    print(line)
+# return exit status to IPython
+logfile.write("Exiting with status " + str(sts[1]) + ".\n\n")
+logfile.close()
+if sts[1] > 0:
+    exit(sts[1])
 
-# This should requeue the frame if failed
-if sts[1] != 0:
-	print("Requeueing frame...")
-	os.kill(os.getppid(), signal.SIGINT)
-	exit(1)
-else:
-	#if DRQUEUE_OS != "WINDOWS" then:
-	# The frame was rendered properly
-	# We don't know the output image name. If we knew we could set this correctly
-	# chown_block RF_OWNER RD/IMAGE DRQUEUE_FRAME BLOCK 
-
-	# change userid and groupid
-	#chown 1002:1004 $SCENE:h/*
-	print("Finished.")
-#
-# Notice that the exit code of the last command is received by DrQueue
-#
