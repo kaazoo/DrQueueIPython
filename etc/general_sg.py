35e918395ca8f5d4edbaa71ebb96ce5241acf14b
Andreas Schroeder <andreas@drqueue.org>
2011-08-15 23:17:56 +0200

    update render template for general jobs

--- etc/general_sg.py
+++ etc/general_sg.py
@@ -1,81 +1,44 @@
-#
-# THIS IS A PYTHON SCRIPT FILE
-# 
-# Default configuration for general script generator
-# 
-# Python variables
-# GENSCRIPT, OWNER_UID, OWNER_GID
-# 
-# shell variables
-# DRQUEUE_BLOCKSIZE, DRQUEUE_COMPID, DRQUEUE_ENDFRAME, DRQUEUE_ETC, DRQUEUE_FRAME,
-# DRQUEUE_JOBID, DRQUEUE_JOBNAME, DRQUEUE_OS, DRQUEUE_OWNER, DRQUEUE_PADFRAME, 
-# DRQUEUE_PADFRAMES, DRQUEUE_STARTFRAME, DRQUEUE_STEPFRAME
-#
-
-#
-# For platform dependend environment setting a form like this
-# can be used :
-#
-# if DRQUEUE_OS == "LINUX":
-#    # Environment for Linux
-# elsif DRQUEUE_OS == "IRIX":
-#    # Environment for Irix
-# else
-#    # Some error messages
-#
+# -*- coding: utf-8 -*-
 
 import os,signal,subprocess,sys
-
-os.umask(0)
-
-# fetch DrQueue environment
-DRQUEUE_BLOCKSIZE = int(os.getenv("DRQUEUE_BLOCKSIZE"))
-DRQUEUE_COMPID = int(os.getenv("DRQUEUE_COMPID"))
-DRQUEUE_ENDFRAME = int(os.getenv("DRQUEUE_ENDFRAME"))
-DRQUEUE_ETC = os.getenv("DRQUEUE_ETC")
-DRQUEUE_FRAME = int(os.getenv("DRQUEUE_FRAME"))
-DRQUEUE_JOBID = int(os.getenv("DRQUEUE_JOBID"))
-DRQUEUE_JOBNAME = os.getenv("DRQUEUE_JOBNAME")
-DRQUEUE_OS = os.getenv("DRQUEUE_OS")
-DRQUEUE_OWNER = os.getenv("DRQUEUE_OWNER")
-DRQUEUE_PADFRAME = int(os.getenv("DRQUEUE_PADFRAME"))
-DRQUEUE_PADFRAMES = int(os.getenv("DRQUEUE_PADFRAMES"))
-DRQUEUE_STARTFRAME = int(os.getenv("DRQUEUE_STARTFRAME"))
-DRQUEUE_STEPFRAME = int(os.getenv("DRQUEUE_STEPFRAME"))
+import os.path
+from time import strftime,localtime
 
 
-if DRQUEUE_OS == "WINDOWS":
-	# convert to windows path with drive letter
-	GENSCRIPT = subprocess.Popen(["cygpath.exe", "-w "+GENSCRIPT], stdout=subprocess.PIPE).communicate()[0]
+# external variables in upper case:
+# DRQUEUE_OS
+# DRQUEUE_ETC
+# DRQUEUE_SCENEFILE
+# DRQUEUE_FRAME
+# DRQUEUE_BLOCKSIZE
+# DRQUEUE_ENDFRAME
+# DRQUEUE_LOGFILE
 
-BLOCK = DRQUEUE_FRAME + DRQUEUE_BLOCKSIZE - 1
+# range to render
+block = DRQUEUE_FRAME + DRQUEUE_BLOCKSIZE - 1
+if block > DRQUEUE_ENDFRAME:
+	block = DRQUEUE_ENDFRAME
 
-if BLOCK > DRQUEUE_ENDFRAME:
-	BLOCK = DRQUEUE_ENDFRAME
+command = DRQUEUE_SCENEFILE + " " + DRQUEUE_FRAME
 
+# write output to logfile
+logfile = open(DRQUEUE_LOGFILE, "ab")
+logfile.write("Log started at " + strftime("%a, %d %b %Y %H:%M:%S", localtime()) + "\n\n")
+logfile.write(command+"\n")
+logfile.flush()
 
-command = GENSCRIPT+" "+str(DRQUEUE_FRAME)
+# check scenefile
+if os.path.isfile(DRQUEUE_SCENEFILE) == False:
+    logfile.write("Scenefile was not found. Exiting with status 1.\n\n")
+    logfile.close()
+    exit(1)
 
-print(command)
-sys.stdout.flush()
-
-p = subprocess.Popen(command, shell=True)
+# run renderer and wait for finish
+p = subprocess.Popen(command, shell=True, stdout=logfile, stderr=subprocess.STDOUT)
 sts = os.waitpid(p.pid, 0)
 
-# This should requeue the frame if failed
-if sts[1] != 0:
-	print("Requeueing frame...")
-	os.kill(os.getppid(), signal.SIGINT)
-	exit(1)
-else:
-	#if DRQUEUE_OS != "WINDOWS" then:
-	# The frame was rendered properly
-	# We don't know the output image name. If we knew we could set this correctly
-	# chown_block RF_OWNER RD/IMAGE DRQUEUE_FRAME BLOCK 
-
-	# change userid and groupid
-	#chown 1002:1004 $SCENE:h/*
-	print("Finished.")
-#
-# Notice that the exit code of the last command is received by DrQueue
-#
+# return exit status to IPython
+logfile.write("Exiting with status " + str(sts[1]) + ".\n\n")
+logfile.close()
+if sts[1] > 0:
+    exit(sts[1])
