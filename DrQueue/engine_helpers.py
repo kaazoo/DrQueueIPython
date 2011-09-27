2d7bf32d3ff54c57110ce180a330fa7bc594aca9
Andreas Schroeder <andreas@drqueue.org>
2011-09-27 23:10:28 +0200

    use methods of helper class

--- DrQueue/engine_helpers.py
+++ DrQueue/engine_helpers.py
@@ -15,53 +15,66 @@ import DrQueue
 from computer import Computer as DrQueueComputer
 
 
-def calc_block(frame, endframe, size):
-    """Calculate block to render."""
-    block = frame + size - 1
-    if block > endframe:
-    	  block = endframe
-    return block
+class Helper():
+    """Object providing some helper methods for engines."""
+    def __init__(self, env_dict):
+        self.logfile = self.openlog(env_dict['DRQUEUE_LOGFILE'])
+        self.scenefile = env_dict['DRQUEUE_SCENEFILE']
 
-def replace_stdpath_with_driveletter(DRQUEUE_SCENEFILE, drive):
-    """Replace DrQueue standard Unix path with specific Windows drive letter."""
-    DRQUEUE_SCENEFILE = DRQUEUE_SCENEFILE.replace('/usr/local/drqueue', drive)
-    DRQUEUE_SCENEFILE = DRQUEUE_SCENEFILE.replace('/', '\\')
-    return DRQUEUE_SCENEFILE
+    def calc_block(self, frame, endframe, size):
+        """Calculate block to render."""
+        block = frame + size - 1
+        if block > endframe:
+        	  block = endframe
+        return block
 
-def openlog(file):
-    """Open logfile and write header."""
-    logfile = open(file, "ab")
-    logfile.write("Log started at " + strftime("%a, %d %b %Y %H:%M:%S", localtime()) + ".\n")
-    logfile.write("Running on " + DrQueueComputer.get_hostname() + " under " + DrQueueComputer.get_os() + ".\n\n")
-    return logfile
+    def replace_stdpath_with_driveletter(self, path, drive):
+        """Replace DrQueue standard Unix path with specific Windows drive letter."""
+        path = path.replace('/usr/local/drqueue', drive)
+        path = path.replace('/', '\\')
+        return path
 
-def check_scenefile(logfile, file):
-    """Check if scenefile is existing."""
-    if os.path.isfile(file) == False:
-        message = "Scenefile was not found."
-        logfile.write(message)
-        logfile.close()
-        raise ValueError(message)
-        return False
-        
-def run_command(logfile, command):
-    """Run command in shell."""
-    try:
-        p = subprocess.Popen(command, shell=True, stdout=logfile, stderr=subprocess.STDOUT)
-    except OSError as (errno, strerror):
-        message = "OSError({0}) while executing renderer: {1}\n".format(errno, strerror)
-        logfile.write(message)
-        logfile.close()
-        raise OSError(message)
-        return False
-    p.wait()
-    return p.returncode
+    def openlog(self, file):
+        """Open logfile and write header."""
+        logfile = open(file, "ab")
+        logfile.write("Log started at " + strftime("%a, %d %b %Y %H:%M:%S", localtime()) + ".\n")
+        logfile.write("Running on " + DrQueueComputer.get_hostname() + " under " + DrQueueComputer.get_os() + ".\n\n")
+        return logfile
 
-def return_to_ipython(logfile, returncode):
-    """Return exit status to IPython."""
-    logfile.write("Exiting with status " + str(returncode) + ".\n\n")
-    logfile.close()
-    if returncode > 0:
-        return False
-    else:
+    def log_write(self, message):
+        """Write message to logfile."""
+        self.logfile.write(message)
+        self.logfile.flush()
         return True
+
+    def check_scenefile(self):
+        """Check if scenefile is existing."""
+        if os.path.isfile(self.scenefile) == False:
+            message = "Scenefile was not found."
+            self.logfile.write(message)
+            self.logfile.close()
+            raise ValueError(message)
+            return False
+
+    def run_command(self, command):
+        """Run command in shell."""
+        try:
+            p = subprocess.Popen(command, shell=True, stdout=self.logfile, stderr=subprocess.STDOUT)
+        except OSError as (errno, strerror):
+            message = "OSError({0}) while executing renderer: {1}\n".format(errno, strerror)
+            self.logfile.write(message)
+            self.logfile.close()
+            raise OSError(message)
+            return False
+        p.wait()
+        return p.returncode
+
+    def return_to_ipython(self, returncode):
+        """Return exit status to IPython."""
+        self.logfile.write("Exiting with status " + str(returncode) + ".\n\n")
+        self.logfile.close()
+        if returncode > 0:
+            return False
+        else:
+            return True
+
