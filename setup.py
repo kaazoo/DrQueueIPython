06852495f900d51fd37d746eb76f609b712aba41
Andreas Schroeder <andreas@drqueue.org>
2011-12-04 19:29:22 +0100

    support custom base path, owner and group

--- setup.py
+++ setup.py
@@ -1,13 +1,26 @@
 import ez_setup
 ez_setup.use_setuptools()
 
-import os, glob, shutil, sys
+import os, glob, shutil, sys, pwd, grp
 from setuptools import setup
 from distutils.core import setup, Command
 
 
 def read(fname):
+    """Read file contents."""
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+def recursive_chown(path, uid, gid):
+    """Do chown recursively."""
+    os.chown(path, uid, gid)
+    for item in os.listdir(path):
+        itempath = os.path.join(path, item)
+        if os.path.isfile(itempath):
+            os.chown(itempath, uid, gid)
+        elif os.path.isdir(itempath):
+            os.chown(itempath, uid, gid)
+            recursive_chown(itempath, uid, gid)
 
 
 class CreateDrQueueWorkDirs(Command):
@@ -15,24 +28,37 @@ class CreateDrQueueWorkDirs(Command):
 
     description = "Create working directories for DrQueue."
 
-    user_options = []
+    bp_option = ("basepath=", None, "Base path for DrQueue work directories. Default is '/usr/local/drqueue'.")
+    o_option = ("owner=", None, "Owner for directories.")
+    g_option = ("group=", None, "Group for directories.")
+    user_options = [bp_option, o_option, g_option]
     boolean_options = []
 
     def initialize_options(self):
         self._dir = os.getcwd()
+        self.basepath = None
+        self.owner = None
+        self.group = None
 
     def finalize_options(self):
         pass
 
     def run(self):
-        if sys.platform.startswith('win'):
+
+        # Windows lacks Unix functionality
+        if sys.platform.startswith("win"):
             print("Sorry, but creating paths on Windows is curently not supported.")
             sys.exit(1)
 
-        drqueue_root = "/usr/local/drqueue"
+        # set to user-supplied path is available
+        if self.basepath != None:
+            drqueue_root = self.basepath
+        else:
+            drqueue_root = "/usr/local/drqueue"
 
-        print("Creating DrQueue work directories in " + drqueue_root + ".")
+        print("Creating DrQueue work directories in '" + drqueue_root + "'.")
 
+        # create directory structure
         if not os.path.exists(drqueue_root):
             os.makedirs(drqueue_root)
         drqueue_etc = os.path.join(drqueue_root, "etc")
@@ -48,9 +74,18 @@ class CreateDrQueueWorkDirs(Command):
         if not os.path.exists(drqueue_tmp):
             os.mkdir(drqueue_tmp)
 
+        # copy template files
         templates = os.path.join(self._dir, "etc", "*.py")
         for template in glob.glob(templates):
             shutil.copy(template, drqueue_etc)
+
+        # set to user-supplied user / group
+        if self.owner != None:
+            uid = pwd.getpwnam(self.owner)[2]
+            recursive_chown(drqueue_root, uid, -1)
+        if self.group != None:
+            gid = grp.getgrnam(self.group)[2]
+            recursive_chown(drqueue_root, -1, gid)
 
         print("\nAdd the following environment variables to your user profile:")
         print("DRQUEUE_ROOT=" + drqueue_root)
