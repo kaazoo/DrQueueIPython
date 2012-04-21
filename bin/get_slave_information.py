05ebd02a4489102f38084143de64a21d57e254b5
Andreas Schroeder <andreas@drqueue.org>
2012-04-21 23:02:49 +0200

    get engine_id from running engine instance

--- bin/get_slave_information.py
+++ bin/get_slave_information.py
@@ -3,6 +3,7 @@ import os
 import socket
 import DrQueue
 from DrQueue import Computer as DrQueueComputer
+from IPython.config.application import Application
 
 
 if "DRQUEUE_SLAVE" in os.environ:
@@ -10,17 +11,26 @@ if "DRQUEUE_SLAVE" in os.environ:
 else:
     SLAVE_IP = socket.gethostbyname(socket.getfqdn())
 
-IPENGINE_LOGPATH = os.path.join(os.environ["DRQUEUE_ROOT"], "logs", "ipengine_startup_" + SLAVE_IP + ".log")
-IPENGINE_LOGFILE = open(IPENGINE_LOGPATH, "ab")
+IPENGINE_STARTUP_LOGPATH = os.path.join(os.environ["DRQUEUE_ROOT"], "logs", "ipengine_startup_" + SLAVE_IP + ".log")
+IPENGINE_STARTUP_LOGFILE = open(IPENGINE_STARTUP_LOGPATH, "ab")
+
+# connect to current IPython engine instance
+app = Application.instance()
+# get engine_id
+engine_id = app.engine.id
 
 # get computer information
 engine = DrQueueComputer()
 # set creation time
 engine['created_at'] = int(time.time())
+# set engine_id
+engine['engine_id'] = engine_id
 # store entry in database
 DrQueueComputer.store_db(engine)
 
-IPENGINE_LOGFILE.write("Computer info: \n")
-IPENGINE_LOGFILE.write(str(engine) + "\n")
+IPENGINE_STARTUP_LOGFILE.write("\nComputer info: \n")
+IPENGINE_STARTUP_LOGFILE.write(str(engine) + "\n")
 
-IPENGINE_LOGFILE.close
+# flush buffers
+IPENGINE_STARTUP_LOGFILE.flush()
+os.fsync(IPENGINE_STARTUP_LOGFILE.fileno())
