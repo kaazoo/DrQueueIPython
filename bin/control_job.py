29e7e3778a29de7e9a4c5f61b60a6de139140dc3
Andreas Schroeder <andreas@drqueue.org>
2013-01-25 23:47:03 +0100

    use argparse instead of optparse support single action on all known jobs support action on single task of certain job

--- bin/control_job.py
+++ bin/control_job.py
@@ -2,7 +2,7 @@
 
 """
 Control existing jobs
-Copyright (C) 2011 Andreas Schroeder
+Copyright (C) 2011-2013 Andreas Schroeder
 
 This file is part of DrQueue.
 
@@ -10,7 +10,7 @@ Licensed under GNU General Public License version 3. See LICENSE for details.
 """
 
 import os, sys
-from optparse import OptionParser
+import argparse
 import DrQueue
 from DrQueue import Job as DrQueueJob
 from DrQueue import Client as DrQueueClient
@@ -18,85 +18,92 @@ from DrQueue import Client as DrQueueClient
 
 def main():
     # parse arguments
-    parser = OptionParser()
-    parser.usage = "%prog [options] -n name"
-    parser.add_option("-n", "--name ",
-                      dest="name", help="name of job")
-    parser.add_option("-i", "--id ",
-                      dest="id", default=0, help="id of job")
-    parser.add_option("-s", "--stop",
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-n", "--name ",
+                      dest="name", default="", help="name of job")
+    parser.add_argument("-i", "--id ",
+                      dest="id", default=0, help="id of job ('ALL' for all jobs)")
+    parser.add_argument("--tid ",
+                      dest="tid", default=0, help="id of task ('ALL' for all tasks)")
+    parser.add_argument("-s", "--stop",
                       action="store_true", dest="stop", default=False, help="stop the job")
-    parser.add_option("-k", "--kill",
+    parser.add_argument("-k", "--kill",
                       action="store_true", dest="kill", default=False, help="stop the job (be careful, running tasks will be killed)")
-    parser.add_option("-d", "--delete",
+    parser.add_argument("-d", "--delete",
                       action="store_true", dest="delete", default=False, help="delete the job (be careful, no confirmation asked)")
-    parser.add_option("-c", "--continue",
+    parser.add_argument("-c", "--continue",
                       action="store_true", dest="cont", default=False, help="continue a previously stopped job")
-    parser.add_option("-r", "--rerun",
-                      action="store_true", dest="rerun", default=False, help="rerun job")
-    parser.add_option("-R", "--rerun_task",
-                      action="store_true", dest="rerun_task", default=False, help="rerun task")
-    parser.add_option("-F", "--rerun_interrupted_tasks",
+    parser.add_argument("-r", "--rerun",
+                      action="store_true", dest="rerun", default=False, help="rerun job with ID 'id'")
+    parser.add_argument("-R", "--rerun_task",
+                      action="store_true", dest="rerun_task", default=False, help="rerun task with ID 'tid' of job identified by ID 'id' or NAME 'n'")
+    parser.add_argument("-F", "--rerun_interrupted_tasks",
                       action="store_true", dest="rerun_interrupted_tasks", default=False, help="rerun interrupted tasks")
-    parser.add_option("-t", "--status",
+    parser.add_argument("-t", "--status",
                       action="store_true", dest="status", default=False, help="show the status of the job")
-    parser.add_option("-v", "--verbose",
+    parser.add_argument("-v", "--verbose",
                       action="store_true", dest="verbose", default=False, help="verbose output")
-    (options, args) = parser.parse_args()
+    args = parser.parse_args()
+
 
     # initialize DrQueue client
     client = DrQueueClient()
 
-    # get job information
-    if options.id == 0:
-        job = DrQueueJob.query_job_by_name(options.name)
+    # get job information by name
+    if (args.id == 0) and (args.name != ""):
+        jobs = []
+        job = client.query_job_by_name(args.name)
         if job == None:
             print("Specified job does not exist.")
             sys.exit(1)
-        job_id = job['_id']
-        job_name = options.name
-    else:
-        if not options.rerun_task:
-            job_id = options.id
-            job = DrQueueJob.query_db(job_id)
+        jobs.append(job)
+    # get job information by id
+    elif (args.id != 0) and (args.name == ""):
+        if args.id == "ALL":
+            jobs = client.query_job_list()
+        else:
+            jobs = []
+            job = client.query_job_by_id(args.id)
             if job == None:
                 print("Specified job does not exist.")
                 sys.exit(1)
-            job_name = job['name']
+            jobs.append(job)
+    # id and name are missing
+    else:
+        parser.print_help()
+        sys.exit(1)
 
-    # run specified action on job
-    if options.stop:
-        client.job_stop(job_id)
-        print("Job %s has been stopped." % job_name)
-        return
-    if options.kill:
-        client.job_kill(job_id)
-        print("Job %s has been killed." % job_name)
-        return
-    if options.delete:
-        client.job_delete(job_id)
-        print("Job %s has been deleted." % job_name)
-        return
-    if options.cont:
-        client.job_continue(job_id)
-        print("Job %s is running again." % job_name)
-        return
-    if options.rerun:
-        client.job_rerun(job_id)
-        print("Job %s is running another time." % job_name)
-        return
-    if options.rerun_task:
-        client.task_rerun(options.id)
-        print("Task %s is running another time." % options.id)
-        return
-    if options.rerun_interrupted_tasks:
-        client.job_rerun_interrupted_tasks(job_id)
-        print("Interrupted tasks of job %s are running another time." % job_name)
-        return
-    if options.status:
-        status = client.job_status(job_id)
-        print("The status of job %s is \"%s\"" % (job_name, status))
-        return
+    # work on task id
+    if args.tid != 0:
+        if args.rerun_task:
+            client.task_rerun(args.tid)
+            print("Task %s of job %s is running another time." % (args.tid, jobs[0]['name']))
+            return
+
+    # run specified action on job(s)
+    for job in jobs:
+        if args.stop:
+            client.job_stop(job['_id'])
+            print("Job %s has been stopped.\n" % job['name'])
+        if args.kill:
+            client.job_kill(job['_id'])
+            print("Job %s has been killed.\n" % job['name'])
+        if args.delete:
+            client.job_delete(job['_id'])
+            print("Job %s has been deleted.\n" % job['name'])
+        if args.cont:
+            client.job_continue(job['_id'])
+            print("Job %s is running again.\n" % job['name'])
+        if args.rerun:
+            client.job_rerun(job['_id'])
+            print("Job %s is running another time.\n" % job['name'])
+        if args.rerun_interrupted_tasks:
+            client.job_rerun_interrupted_tasks(job['_id'])
+            print("Interrupted tasks of job %s are running another time.\n" % job['name'])
+        if args.status:
+            status = client.job_status(job['_id'])
+            print("The status of job %s is \"%s\"\n" % (job['name'], status))
+    return
 
 if __name__ == "__main__":
     main()
