185dd31f030846c0ae63ee19ac2ae01039ca0cce
Andreas Schroeder <andreas@drqueue.org>
2013-10-10 23:00:37 +0200

    remove unused files

--- _legacy/bin/slave_win.sh
+++
@@ -1,6 +0,0 @@
-[ -z $DRQUEUE_ROOT ]   && export DRQUEUE_ROOT=/usr/local/drqueue
-[ -z $DRQUEUE_MASTER ] && export DRQUEUE_MASTER=127.0.0.1
-export CYGWIN=server
-export PATH=$PATH:$DRQUEUE_ROOT/bin
-
-slave.exe.CYGWIN_NT-5.1.i686 -o
