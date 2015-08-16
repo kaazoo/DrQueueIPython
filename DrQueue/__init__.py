4491f13ee7aaa371824b096d72ac167cba8db79a
Andreas Schroeder <andreas@drqueue.org>
2015-08-16 20:30:52 +0200

    generate filename from renderer name

--- DrQueue/__init__.py
+++ DrQueue/__init__.py
@@ -38,46 +38,7 @@ def check_renderer_support(renderer):
 
 def get_rendertemplate(renderer):
     """Return template filename from renderer name"""
-    filename = ""
-    if renderer == '3delight':
-        filename = '3delight_sg.py'
-    if renderer == '3dsmax':
-        filename = '3dsmax_sg.py'
-    if renderer == 'aftereffects':
-        filename = 'aftereffects_sg.py'
-    if renderer == 'aqsis':
-        filename = 'aqsis_sg.py'
-    if renderer == 'blender':
-        filename = 'blender_sg.py'
-    if renderer == 'cinema4d':
-        filename = 'cinema4d_sg.py'
-    if renderer == 'general':
-        filename = 'general_sg.py'
-    if renderer == 'lightwave':
-        filename = 'lightwave_sg.py'
-    if renderer == 'luxrender':
-        filename = 'luxrender_sg.py'
-    if renderer == 'mantra':
-        filename = 'mantra_sg.py'
-    if renderer == 'maya':
-        filename = 'maya_sg.py'
-    if renderer == 'mentalray':
-        filename = 'mentalray_sg.py'
-    if renderer == 'nuke':
-        filename = 'nuke_sg.py'
-    if renderer == 'pixie':
-        filename = 'pixie_sg.py'
-    if renderer == 'shake':
-        filename = 'shake_sg.py'
-    if renderer == 'terragen':
-        filename = 'terragen_sg.py'
-    if renderer == 'turtle':
-        filename = 'turtle_sg.py'
-    if renderer == 'vray':
-        filename = 'vray_sg.py'
-    if renderer == 'xsi':
-        filename = 'xsi_sg.py'
-    return filename 
+    return renderer + '_sg.py'
 
 
 def get_osname():
